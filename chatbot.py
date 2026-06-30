5dcd2cc933c2b013d5892f0321d80bd1e8c1ac61
samuelafanou4-source <samuelafanou4@gmail.com>
2026-06-30 16:28:52 +0000

    configuration du middleware dans chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from supabase import create_client, Client
+from fastapi.middleware.cors import CORSMiddleware
 from google import genai  # <-- Le nouveau package officiel de Google
 from typing import Optional
 
@@ -25,6 +26,22 @@ ai_client = genai.Client(
     api_key=settings.gemini_api_key_1,
     http_options={"api_version": "v1"}
 )
+
+# ==========================================
+# 2. CONFIGURATION DU MIDDLEWARE (CORS)
+# ==========================================
+
+# Configuration du CORS (Cross-Origin Resource Sharing) pour permettre au Frontend 
+# (situé sur un autre domaine/port) d'interroger cette API sans blocage de sécurité navigateur.
+router.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # Autorise toutes les origines (utile en Hackathon, à restreindre en prod)
+    allow_credentials=True, # Autorise l'envoi de cookies ou de headers d'authentification
+    allow_methods=["*"], # Autorise toutes les méthodes HTTP (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"], # Autorise tous les en-têtes HTTP de requêtes
+)
+
+
 # Modèles de requêtes
 class ChatRequest(BaseModel):
     message: str
